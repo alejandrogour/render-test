d306042d1c6d2149fce96ec7b91fe651de713cef
Ale <alee@ff8.dev>
2025-07-18 15:26:40 +0200

    eslint config and plugin set

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,33 +14,33 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    important: Boolean
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const notes = [
-    new Note({ content: 'HTML is easy', important: true }),
-    new Note({ content: 'CSS is fun', important: false }),
-    new Note({ content: 'JavaScript is powerful', important: true })
+  new Note({ content: 'HTML is easy', important: true }),
+  new Note({ content: 'CSS is fun', important: false }),
+  new Note({ content: 'JavaScript is powerful', important: true })
 ]
 
 Promise.all(notes.map(note => note.save()))
-    .then(() => {
-        Note.find({}).then(result => {
-            result.forEach(note => console.log(note))
-            mongoose.connection.close()
-        })
+  .then(() => {
+    Note.find({}).then(result => {
+      result.forEach(note => console.log(note))
+      mongoose.connection.close()
     })
-    .catch(err => {
-        console.error(err)
-        mongoose.connection.close()
-    })
+  })
+  .catch(err => {
+    console.error(err)
+    mongoose.connection.close()
+  })
 
 
 
